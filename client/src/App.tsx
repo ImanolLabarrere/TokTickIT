00ec59b62bdd969d9192aa8e9280a34b4b10f6d2
ImanolLabarrere <imanol.labarrere@gmail.com>
2026-09-08 14:11:35 +0700

    feat: My Tickets list with search, filters, sort, and pagination (Issue 4)

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,18 +1,25 @@
+import { useState } from "react";
 import { RequesterProvider, useRequester } from "./context/RequesterContext.js";
 import RequesterSelection from "./components/RequesterSelection.js";
-import AppShell from "./components/AppShell.js";
+import AppShell, { AppView } from "./components/AppShell.js";
 import CreateTicket from "./components/CreateTicket.js";
+import MyTickets from "./components/MyTickets.js";
 
 function AppContent() {
   const { currentRequester } = useRequester();
+  const [view, setView] = useState<AppView>("my-tickets");
 
   if (!currentRequester) {
     return <RequesterSelection />;
   }
 
   return (
-    <AppShell>
-      <CreateTicket />
+    <AppShell activeView={view} onNavigate={setView}>
+      {view === "my-tickets" ? (
+        <MyTickets onCreateTicket={() => setView("create-ticket")} />
+      ) : (
+        <CreateTicket />
+      )}
     </AppShell>
   );
 }
